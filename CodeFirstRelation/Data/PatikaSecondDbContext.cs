02f010dea6beaafcfd0f1ccf5988aa07794f066f
EmreCanTERKAN <emrecanterkan06@gmail.com>
2024-10-03 17:10:09 +0300

    Add project files.

---
+++ CodeFirstRelation/Data/PatikaSecondDbContext.cs
@@ -0,0 +1,24 @@
+﻿using CodeFirstRelation.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstRelation.Data
+{
+    public class PatikaSecondDbContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Post> Users { get; set; }
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-GRDREHV\SQLEXPRESS; Database=Deneme435345; TrustServerCertificate=True; Trusted_Connection=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ CodeFirstRelation/Program.cs
@@ -0,0 +1,28 @@
+using CodeFirstRelation.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<PatikaSecondDbContext>();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
